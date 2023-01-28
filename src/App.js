2de590bbd8bb60055ff2df7f9dcc08b776ef6533
ykhoge <kasaharayuuuuu@gmail.com>
2023-01-28 08:09:03 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { RecoilRoot } from "recoil";
 import { UserProvider } from "./provider/UserProvider";
+import { RecoilRoot } from "recoil";
 import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
   return (
-    <UserProvider>
-      <Router />
-    </UserProvider>
+    <RecoilRoot>
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </RecoilRoot>
   );
 }
